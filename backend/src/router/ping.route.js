557f157c315acf99007515ed062fd888a0829dff
Chinmoy <160400981+chinmoy-2004@users.noreply.github.com>
2025-07-08 21:17:35 +0530

    Create ping.route.js

---
+++ backend/src/router/ping.route.js
@@ -0,0 +1,9 @@
+// routes/ping.route.js
+import express from 'express';
+const router = express.Router();
+
+router.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
+
+export default router;
